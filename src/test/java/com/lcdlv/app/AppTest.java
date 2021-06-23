63a43692057f1a71fdda813245c10cc70ce29a4a
BLANCHARD Alexis <alexis-externe.blanchard@dalkia.fr>
2021-06-23 11:21:05 +0200

    Setup project

---
+++ src/test/java/com/lcdlv/app/AppTest.java
@@ -0,0 +1,16 @@
+package com.lcdlv.app;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+class AppTest {
+
+    @Test
+    void shouldPass() {
+        assertTrue(true);
+    }
+
+}
