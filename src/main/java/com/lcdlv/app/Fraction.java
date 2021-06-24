06e83c1bd68d813213493f9b81e277608cec5552
BLANCHARD Alexis <alexis-externe.blanchard@dalkia.fr>
2021-06-24 17:52:52 +0200

    Change types of numerator & denominator from int to BigInteger

--- src/main/java/com/lcdlv/app/Fraction.java
+++ src/main/java/com/lcdlv/app/Fraction.java
@@ -1,32 +1,55 @@
 package com.lcdlv.app;
 
+import java.math.BigInteger;
+import java.util.Objects;
+
+
 public class Fraction {
 
-    private int numerator;
-    private int denominator;
+    private BigInteger numerator;
+    private BigInteger denominator;
 
-    private Fraction(int numerator, int denominator) {
+    private Fraction(BigInteger numerator, BigInteger denominator) {
         this.numerator = numerator;
         this.denominator = denominator;
     }
 
     public static Fraction of(int numerator, int denominator) {
-        if (denominator == 0) {
+        return of(BigInteger.valueOf(numerator), BigInteger.valueOf(denominator));
+    }
+
+    public static Fraction of(BigInteger numerator, BigInteger denominator) {
+        if (denominator.intValue() == 0) {
             throw new IllegalArgumentException("Denominator value 0, is not legal");
         }
         return new Fraction(numerator, denominator).simplify();
     }
 
     public static Fraction of(int numerator) {
-        return new Fraction(numerator, 1).simplify();
+        return new Fraction(BigInteger.valueOf(numerator), BigInteger.valueOf(1)).simplify();
     }
 
+    /**
+     * a   c   ad + cb
+     * _ + _ = _______
+     * b   d     bd
+     */
     public Fraction add(Fraction fraction) {
-        return Fraction.of((this.numerator * fraction.denominator) + (fraction.numerator * this.denominator), fraction.denominator * this.denominator);
+        BigInteger ad = this.numerator.multiply(fraction.denominator);
+        BigInteger cb = fraction.numerator.multiply(this.denominator);
+        BigInteger bd = this.denominator.multiply(fraction.denominator);
+        return Fraction.of(ad.add(cb), bd);
     }
 
+    /**
+     * a   c    a * c
+     * _ * _ = _______
+     * b   d    b * d
+     */
     public Fraction multiply(Fraction fraction) {
-        return of(this.numerator * fraction.numerator, this.denominator * fraction.denominator);
+        BigInteger ac = this.numerator.multiply(fraction.numerator);
+        BigInteger bd = this.denominator.multiply(fraction.denominator);
+        return of(ac, bd);
     }
 
     public Fraction divide(Fraction fraction) {
@@ -42,7 +65,8 @@ public class Fraction {
     }
 
     private Fraction opposite() {
-        return of(-this.numerator, this.denominator);
+        BigInteger numeratorOpposite = this.numerator.multiply(BigInteger.valueOf(-1));
+        return of(numeratorOpposite, this.denominator);
     }
 
     @Override
@@ -52,29 +76,29 @@ public class Fraction {
 
         Fraction fraction = (Fraction) o;
 
-        if (numerator != fraction.numerator) return false;
-        return denominator == fraction.denominator;
+        if (!Objects.equals(this.numerator, fraction.numerator)) return false;
+        return Objects.equals(this.denominator, fraction.denominator);
     }
 
     @Override
     public int hashCode() {
-        int result = numerator;
-        result = 31 * result + denominator;
+        int result = this.numerator != null ? this.numerator.hashCode() : 0;
+        result = 31 * result + (this.denominator != null ? this.denominator.hashCode() : 0);
         return result;
     }
 
     @Override
     public String toString() {
-        return numerator + (denominator != 1 ? " / " + denominator : "");
+        return this.numerator.intValue() + (this.denominator.intValue() != 1 ? " / " + this.denominator.intValue() : "");
     }
 
     Fraction simplify() {
-        if (this.denominator < 0) {
-            this.denominator *= -1;
-            this.numerator *= -1;
+        if (this.denominator.signum() < 0) {
+            this.denominator = this.denominator.multiply(BigInteger.valueOf(-1));
+            this.numerator = this.numerator.multiply(BigInteger.valueOf(-1));
         }
-        int gcd = GreaterCommonDivisor.compute(this.numerator, this.denominator);
-        return new Fraction(this.numerator / gcd, this.denominator / gcd);
+        BigInteger gcd = this.numerator.gcd(this.denominator);
+        return new Fraction(this.numerator.divide(gcd), this.denominator.divide(gcd));
     }
 
 }
