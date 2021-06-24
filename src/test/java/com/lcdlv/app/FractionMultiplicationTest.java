e5f3324480a5b703999ae64c4f0897a08a52ca55
BLANCHARD Alexis <alexis-externe.blanchard@dalkia.fr>
2021-06-24 11:11:23 +0200

    Add 2nd fraction multiplication test

--- src/test/java/com/lcdlv/app/FractionMultiplicationTest.java
+++ src/test/java/com/lcdlv/app/FractionMultiplicationTest.java
@@ -8,8 +8,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 class FractionMultiplicationTest {
 
     @Test
-    void shouldMultiplyFractions() {
-        assertThat(of(1,1).multiply(of(1,1))).isEqualTo(of(1,1));
+    void shouldMultiplyFractions_1() {
+        assertThat(of(1, 1).multiply(of(1, 1))).isEqualTo(of(1));
+    }
+
+    @Test
+    void shouldMultiplyFractions_2() {
+        assertThat(of(1, 2).multiply(of(1))).isEqualTo(of(1, 2));
     }
 
 }
