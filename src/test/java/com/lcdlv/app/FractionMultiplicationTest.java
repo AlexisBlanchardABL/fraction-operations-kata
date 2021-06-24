1b38f2e891d458c4521e62b896f793a7592af13d
BLANCHARD Alexis <alexis-externe.blanchard@dalkia.fr>
2021-06-24 11:32:14 +0200

    Parameterize multiplication tests, identity case

--- src/test/java/com/lcdlv/app/FractionMultiplicationTest.java
+++ src/test/java/com/lcdlv/app/FractionMultiplicationTest.java
@@ -1,20 +1,28 @@
 package com.lcdlv.app;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static com.lcdlv.app.Fraction.of;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class FractionMultiplicationTest {
 
-    @Test
-    void shouldMultiplyFractions_1() {
-        assertThat(of(1, 1).multiply(of(1, 1))).isEqualTo(of(1));
+    @ParameterizedTest(name = "{0} multiplied by {1} => {2}")
+    @MethodSource("multiplyFractionsWhenIdentity")
+    void shouldMultiplyFractionsWhenIdentity(Fraction fraction, Fraction fractionToMultiply, Fraction expectedResult) {
+        assertThat(fraction.multiply(fractionToMultiply)).isEqualTo(expectedResult);
     }
 
-    @Test
-    void shouldMultiplyFractions_2() {
-        assertThat(of(1, 2).multiply(of(1))).isEqualTo(of(1, 2));
+    private static Stream<Arguments> multiplyFractionsWhenIdentity() {
+        return Stream.of(
+                Arguments.of(of(1, 2), of(1), of(1, 2)),
+                Arguments.of(of(1), of(1), of(1)),
+                Arguments.of(of(5, 2), of(1), of(5, 2))
+        );
     }
 
 }
