1ff38ed7f7d8947761384b457fda557dc98b0023
BLANCHARD Alexis <alexis-externe.blanchard@dalkia.fr>
2021-10-18 10:00:19 +0200

    Add user has no friend empty list trip

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -2,37 +2,54 @@ package org.craftedsw.tripservicekata.trip;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MockitoExtension.class)
 public class TripServiceTest {
 
     private TripService tripService;
 
-    @Mock
-    private User user;
-
-    @BeforeEach
-    void setup() {
-        tripService = new MyTripService();
-    }
 
     @Test
     void shouldThrowAnException() {
-        assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(user));
+        User loggedUser = null;
+        tripService = new MyTripService(loggedUser);
+        User user = new User();
+        assertThrows(UserNotLoggedInException.class, () -> {
+            tripService.getTripsByUser(user);
+        });
+    }
+
+    @Test
+    void shoultReturnEmptyTripList_whenUserHasNoFriend() {
+        // Given
+        User loggedUser = new User();
+        tripService = new MyTripService(loggedUser);
+        User userWithNoFriends = new User();
+        // When
+        List<Trip> trips = tripService.getTripsByUser(userWithNoFriends);
+        // Then
+        assertTrue(trips.isEmpty());
     }
 
 
     private static class MyTripService extends TripService {
+        private final User loggedUser;
+
+        public MyTripService(User loggedUser) {
+            this.loggedUser = loggedUser;
+        }
+
         @Override
         User getLoggedUser() {
-            return null;
+            return loggedUser;
         }
     }
 
