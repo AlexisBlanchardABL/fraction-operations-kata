b03311ddc4168451a9682d1e601089cf815aedd2
BLANCHARD Alexis <alexis-externe.blanchard@dalkia.fr>
2021-10-18 12:43:07 +0200

    1st step

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -5,36 +5,50 @@ import java.util.List;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
+import org.craftedsw.tripservicekata.user.UserRepository;
 import org.craftedsw.tripservicekata.user.UserSession;
 
 public class TripService {
 
-	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<>();
-		User loggedUser = getLoggedUser();
-		boolean isFriend = false;
-		if (loggedUser != null) {
-			for (User friend : user.getFriends()) {
-				if (friend.equals(loggedUser)) {
-					isFriend = true;
-					break;
-				}
-			}
-			if (isFriend) {
-				tripList = getUserTrips(user);
-			}
-			return tripList;
-		} else {
-			throw new UserNotLoggedInException();
-		}
-	}
+    private UserRepository userRepository;
 
-	List<Trip> getUserTrips(User user) {
-		return TripDAO.findTripsByUser(user);
-	}
+    public TripService() {
+        this.userRepository = null;
+    }
 
-	User getLoggedUser() {
-		return UserSession.getInstance().getLoggedUser();
-	}
+    public TripService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
+        List<Trip> tripList = new ArrayList<>();
+        User loggedUser = getLoggedUser();
+        boolean isFriend = false;
+        if (loggedUser != null) {
+            for (User friend : user.getFriends()) {
+                if (friend.equals(loggedUser)) {
+                    isFriend = true;
+                    break;
+                }
+            }
+            if (isFriend) {
+                tripList = getUserTrips(user);
+            }
+            return tripList;
+        } else {
+            throw new UserNotLoggedInException();
+        }
+    }
+
+    List<Trip> getUserTrips(User user) {
+        return TripDAO.findTripsByUser(user);
+    }
+
+    User getLoggedUser() {
+        if (userRepository == null) {
+            return UserSession.getInstance().getLoggedUser();
+        }
+        return userRepository.getSessionUser();
+    }
 
 }
